39e9c55dd3284ac6a1342e67c90a6c90a9b17680
Amit Jadav <amitmjadav007@gmail.com>
2026-05-31 14:54:39 +0530

    Added code

---
+++ .github/main.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "ec2_sg" {
+  name = "github-actions-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami                    = "ami-0685bcc683dadb6b9"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "GitHubActions-EC2"
+  }
+}
